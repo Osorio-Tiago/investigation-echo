3771f69c6a81286f594a66b2c937357ba04af6fc
Ariadna_Ch <ariadna.c2405@gmail.com>
2020-09-28 18:57:43 -0600

    Implementación métodos de Persona

--- src/Demostracion.h
+++ src/Demostracion.h
@@ -24,4 +24,33 @@ public:
     Persona(string nombre, int edad, int id);
     string toString();
 };
+
+//Definición de los métodos
+Persona::Persona()
+{
+    setClassName("Persona");
+    Register("Nombre", &nombre);
+    Register("Edad", &edad);
+    Register("Identificacion", &id);
+
+}
+
+Persona::Persona(string nombre, int edad, int id) {
+    this->nombre = nombre;
+    this->edad = edad;
+    this->id = id;
+}
+
+string Persona::toString() {
+    {
+        ostringstream output;
+        output << "Nombre: " << nombre.value() << endl;
+        output << "Edad: " << edad.value() << endl;
+        output << "Identificacion: " << id.value() << endl;
+
+        return output.str();
+    }
+}
+
+//-----------------------------------------------------------------------------
 #endif //INVESTIGATION_DELTA_DEMOSTRACION_H
